98e17a0abaab7458ffcb1625781cf1e0cbeebf51
yanghua <yanghua1127@gmail.com>
2013-10-05 15:39:43 +0800

    changed service

--- shell/service/cloudHostsUpdater.py
+++ shell/service/cloudHostsUpdater.py
@@ -15,12 +15,9 @@ HOSTS_URL='https://smarthosts.googlecode.com/svn/trunk/hosts'
 LOCAL_HOSTS='/etc/hosts'
 
 def main():
-    """主函数"""
-    #备份文件
     copyfile(LOCAL_HOSTS,'hosts.bak')
     with open(LOCAL_HOSTS,'aw') as hosts:
         hosts.write(os.linesep)
-        #转义windows和unix的换行
         for line in urllib.urlopen(HOSTS_URL):
             hosts.write(line.strip()+os.linesep)
 
